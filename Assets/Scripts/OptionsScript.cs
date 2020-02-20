7f3536aacbb4bd1e65c08686648588cb29ba896f
Guilherme Santana <guilhermesanttana@gmail.com>
2020-02-19 23:45:23 -0300

    Assets

---
+++ Assets/Scripts/OptionsScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsScript : MonoBehaviour
+{
+    public GameObject options;
+
+    void Start()
+    {
+        options.SetActive(false);        
+    }
+
+    public void Click()
+    {
+        if (options.activeSelf == false)
+        {
+            options.SetActive(true);
+        }
+        else
+        {
+            options.SetActive(false);
+        }
+
+    }
+
+
+}
